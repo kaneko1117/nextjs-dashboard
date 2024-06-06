3d7e8b29bab10fe0ff1fd1d2870705211e1e6fa5
kaneko1117 <yuta_kaneko@pj100.biz>
2024-06-06 14:06:39 +0900

    ✨loading test

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,7 +16,6 @@ export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
-  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
