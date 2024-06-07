0e99e34a3bd38e962e3409eb9fa160e250e1f3ee
kaneko1117 <yuta_kaneko@pj100.biz>
2024-06-07 18:41:32 +0900

    ✨環境変数

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
