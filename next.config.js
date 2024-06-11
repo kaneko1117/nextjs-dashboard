aa80459dec4f43a183db99e051a58f1aa5f6f9e0
kaneko1117 <yuta_kaneko@pj100.biz>
2024-06-11 17:00:30 +0900

    ✨ Intercepting Routes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
